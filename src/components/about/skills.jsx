c4d0a6ca29df1cbc9e1e148da6cbd87414a1e1d2
JuliaFSO <juliafso@gmail.com>
2023-10-04 15:46:40 -0400

    Update skills

--- src/components/about/skills.jsx
+++ src/components/about/skills.jsx
@@ -1,74 +1,75 @@
 import './about.css';
+import Nextjs from '../../assets/images/nextjs_icon.png';
+import Redux from '../../assets/images/redux_icon.png';
+import Postgresql from '../../assets/images/postgresql_icon.png';
+import Rails from '../../assets/images/rails_icon.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareJs, faReact, faHtml5, faNode, faCss3Alt, faBootstrap } from '@fortawesome/free-brands-svg-icons';
 
 export default function Skills() {
   return (
     <div className='section_skills'>
       <div className='sub_title'>SKILLS</div>
       <div className='skill_column'>
-        <div>
+        <div className='skill_set'>
+          <div className='skill_icon_bkg'>
+            <FontAwesomeIcon icon={faSquareJs} className='skill_icon' style={{ color: '#fff' }} />
+          </div>
           <span className='skill_text'>Javascript</span>
-          <div className='chart_bar'>
-            <span className='item_progress' data-percent="70" style={{width: '70%'}}></span>
-            <span className='percent' style={{right: '30%'}}>70%<span className='arrow'></span></span>
-          </div>
         </div>
-        <div>
+        <div className='skill_set'>
+          <div className='skill_icon_bkg'>
+            <FontAwesomeIcon icon={faReact} className='skill_icon' style={{ color: '#fff' }} />
+          </div>
           <span className='skill_text'>React.js</span>
-          <div className='chart_bar'>
-            <span className='item_progress' data-percent="80" style={{width: '80%'}}></span>
-            <span className='percent' style={{right: '20%'}}>80%<span className='arrow'></span></span>
-          </div>
         </div>
-        <div>
+        <div className='skill_set'>
+          <div className='skill_icon_bkg'>
+            <img src={Nextjs} className='skill_icon' alt='Next.js' />
+          </div>
           <span className='skill_text'>Next.js</span>
-          <div className='chart_bar'>
-            <span className='item_progress' data-percent="40" style={{width: '40%'}}></span>
-            <span className='percent' style={{right: '60%'}}>40%<span className='arrow'></span></span>
-          </div>
         </div>
-        <div>
-          <span className='skill_text'>HTML</span>
-          <div className='chart_bar'>
-            <span className='item_progress' data-percent="90" style={{width: '90%'}}></span>
-            <span className='percent' style={{right: '10%'}}>90%<span className='arrow'></span></span>
+        <div className='skill_set'>
+          <div className='skill_icon_bkg'>
+            <FontAwesomeIcon icon={faNode} className='skill_icon' style={{ color: '#fff' }} />
           </div>
-        </div>
-        <div>
-          <span className='skill_text'>CSS</span>
-          <div className='chart_bar'>
-            <span className='item_progress' data-percent="70" style={{width: '70%'}}></span>
-            <span className='percent' style={{right: '30%'}}>70%<span className='arrow'></span></span>
-          </div>
-        </div>
-      </div>
-      <div className='skill_column'>
-        <div>
-          <span className='skill_text'>Ruby on Rails</span>
-          <div className='chart_bar'>
-            <span className='item_progress' data-percent="60" style={{width: '60%'}}></span>
-            <span className='percent' style={{right: '40%'}}>60%<span className='arrow'></span></span>
-          </div>
-        </div>
-        <div>
-          <span className='skill_text'>PostgreSQL</span>
-          <div className='chart_bar'>
-            <span className='item_progress' data-percent="45" style={{width: '45%'}}></span>
-            <span className='percent' style={{right: '55%'}}>45%<span className='arrow'></span></span>
-          </div>
-        </div>
-        <div>
           <span className='skill_text'>Node.js</span>
-          <div className='chart_bar'>
-            <span className='item_progress' data-percent="50" style={{width: '50%'}}></span>
-            <span className='percent' style={{right: '50%'}}>50%<span className='arrow'></span></span>
-          </div>
         </div>
-        <div>
-          <span className='skill_text'>Redux</span>
-          <div className='chart_bar'>
-            <span className='item_progress' data-percent="35" style={{width: '35%'}}></span>
-            <span className='percent' style={{right: '65%'}}>40%<span className='arrow'></span></span>
+        <div className='skill_set'>
+          <div className='skill_icon_bkg'>
+            <img src={Redux} className='skill_icon' alt='Redux' />
           </div>
+          <span className='skill_text'>Redux</span>
+        </div>
+        <div className='skill_set'>
+          <div className='skill_icon_bkg'>
+            <FontAwesomeIcon icon={faHtml5} className='skill_icon' style={{ color: '#fff' }} />
+          </div>
+          <span className='skill_text'>HTML5</span>
+        </div>
+        <div className='skill_set'>
+          <div className='skill_icon_bkg'>
+            <FontAwesomeIcon icon={faBootstrap} className='skill_icon' style={{ color: '#fff' }} />
+          </div>
+          <span className='skill_text'>Bootstrap</span>
+        </div>
+        <div className='skill_set'>
+          <div className='skill_icon_bkg'>
+            <FontAwesomeIcon icon={faCss3Alt} className='skill_icon' style={{ color: '#fff' }} />
+          </div>
+          <span className='skill_text'>CSS3</span>
+        </div>
+        <div className='skill_set'>
+          <div className='skill_icon_bkg'>
+            <img src={Rails} className='skill_icon' alt='Ruby on Rails' />
+          </div>
+          <span className='skill_text'>Ruby on Rails</span>
+        </div>
+        <div className='skill_set'>
+          <div className='skill_icon_bkg'>
+            <img src={Postgresql} className='skill_icon' alt='PostgreSQL' />
+          </div>
+          <span className='skill_text'>PostgreSQL</span>
         </div>
       </div>
     </div>
